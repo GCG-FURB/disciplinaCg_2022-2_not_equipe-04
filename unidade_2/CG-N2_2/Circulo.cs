f172730f52d50e024816198bea24f5a997c96344
Rossana <rossanadullius@gmail.com>
2022-09-23 20:09:25 -0300

    23/09/2022 - 2 - Funções Pan

---
+++ unidade_2/CG-N2_2/Circulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL;
+
+namespace gcgcg 
+{
+    class Circulo : ObjetoGeometria 
+    {
+        public Circulo(char rotulo, Objeto paiRef, Ponto4D ptoCentro, double raio) : base(rotulo, paiRef)
+        {
+            base.PontosAdicionar(ptoCentro);
+            double angulo = Matematica.GerarPtosCirculoSimetrico(raio);
+            for (int i = 0; i < 72; i++)
+            {
+                base.PontosAdicionar(Matematica.GerarPtosCirculo(angulo, raio));
+                angulo += 35;
+            }
+        }
+
+        protected override void DesenharObjeto()
+        {
+            GL.Begin(base.PrimitivaTipo);
+            foreach (Ponto4D pto in pontosLista)
+            {
+                GL.Vertex2(pto.X, pto.Y);
+            }
+            GL.End();
+        }
+    }
+}
