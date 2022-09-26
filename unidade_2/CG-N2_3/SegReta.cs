694643bc0a8ed71f46f377a65a42658e2cd9f242
Rossana <rossanadullius@gmail.com>
2022-09-26 20:06:43 -0300

    Rossana - 26/09/2022) Exercicio Atividade 3

---
+++ unidade_2/CG-N2_3/SegReta.cs
@@ -0,0 +1,27 @@
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL;
+
+namespace gcgcg {
+    class SegReta : ObjetoGeometria {
+
+        public SegReta(char rotulo, Objeto paiRef, Ponto4D ptoIni, Ponto4D ptoFim) : base(rotulo, paiRef) {
+            base.PontosAdicionar(ptoIni);
+            base.PontosAdicionar(ptoFim);
+        }
+
+        protected override void DesenharObjeto()
+        {
+            GL.Begin(base.PrimitivaTipo);
+            foreach (Ponto4D pto in pontosLista)
+            {
+                GL.Vertex2(pto.X, pto.Y);           
+            }
+            GL.End();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
